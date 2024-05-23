cb81c0acd06fb661b9aa50387c717e90dac39563
Ruhi Kumari Gupta <127949713+ruhigupta2971@users.noreply.github.com>
2024-05-23 21:36:33 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Media from "./routes/Media";
-import LoginSignup from "./components/LoginSignup";
+
 
 export default function App() {
   return (
@@ -12,7 +12,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Media" element={<Media />} />
         <Route path="/About" element={<About />} />
-        <Route path="/login-signup" element={<LoginSignup />} />
+        
       </Routes>
     </div>
   );

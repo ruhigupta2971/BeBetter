f2a2572059334ff17bc03358ba7e9c79ea966f3f
Ruhi Kumari Gupta <127949713+ruhigupta2971@users.noreply.github.com>
2024-05-23 21:45:30 +0530

    Add files via upload

---
+++ src/components/DonationForm.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./DonationForm.css";
+
+function DonationForm({ onClose }) {
+  return (
+    <div className="donation-form-container">
+      <div className="donation-form">
+        <h2>Make a Donation</h2>
+        <form>
+          <input placeholder="Full Name" type="text" required />
+          <input placeholder="Email" type="email" required />
+          <input placeholder="Amount" type="number" required />
+          <input placeholder="Item-type" type="text" required />
+          <textarea placeholder="Message" rows="4"></textarea>
+          <button type="submit">Donate</button>
+        </form>
+        <button className="close-button" onClick={onClose}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default DonationForm;

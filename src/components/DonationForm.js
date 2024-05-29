3cf2e1c8537803a70d8e5d0ba4c6b1aab3e0422d
Ruhi Kumari Gupta <127949713+ruhigupta2971@users.noreply.github.com>
2024-05-29 21:57:44 +0530

    DonationForm.js

--- src/components/DonationForm.js
+++ src/components/DonationForm.js
@@ -9,7 +9,8 @@ function DonationForm({ onClose }) {
         <form>
           <input placeholder="Full Name" type="text" required />
           <input placeholder="Email" type="email" required />
-          <input placeholder="Amount" type="number" required />
+          <input placeholder="Mobile No." type="tel" required />
+          <input placeholder="Full Address" type="text" required />
           <input placeholder="Item-type" type="text" required />
           <textarea placeholder="Message" rows="4"></textarea>
           <button type="submit">Donate</button>

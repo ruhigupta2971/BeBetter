c6e0edec131011b47edde9881e871c9235a1a3d5
Ruhi Kumari Gupta <127949713+ruhigupta2971@users.noreply.github.com>
2024-05-31 00:49:44 +0530

    Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,7 +7,7 @@ import Footer from "../components/Footer";
 function Home() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <Hero
         cName="hero"
         heroImg="https://images.unsplash.com/photo-1469571486292-0ba58a3f068b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"

9279ab03871edd52eeb68179a9efa330d0be4d86
Ruhi Kumari Gupta <127949713+ruhigupta2971@users.noreply.github.com>
2024-05-31 00:51:27 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./routes/About";
 import Media from "./routes/Media";
 import Navbar from "./components/Navbar";
 import LoginSignup from "./components/LoginSignup";
+import Dashboard from './components/Dashboard';
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="/media" element={<Media />} />
         <Route path="/about" element={<About />} />
         <Route path="/loginsignup" element={<LoginSignup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );

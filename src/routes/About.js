8c4a2290bf3bcfae023bbaef4219de13bc78a68e
Ruhi Kumari Gupta <127949713+ruhigupta2971@users.noreply.github.com>
2024-05-31 22:46:46 +0530

    About.js

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,17 +1,23 @@
-import Navbar from "../components/Navbar";
+import React, { useEffect, useState } from "react";
 import Hero from "../components/Hero";
 import AboutImg from "../assets/about.jpg";
 import Footer from "../components/Footer";
 import ContactForm from "../components/ContactForm";
 
 function About() {
+  const [donationCount, setDonationCount] = useState(0);
+
+  useEffect(() => {
+    const count = localStorage.getItem("donationCount") || 0;
+    setDonationCount(parseInt(count, 10));
+  }, []);
+
   return (
     <>
-      {/* <Navbar /> */}
       <Hero
         cName="hero-mid"
         heroImg={AboutImg}
-        text="People Helped By Your Donation"
+        text={`People Helped By Your Donation: ${donationCount}`}
       />
       <ContactForm />
       <Footer />
